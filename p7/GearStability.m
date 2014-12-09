59a3f932e5ec0bc181acf56fd1156e4254654ad3
John de Wasseige <johndewasseige@me.com>
2014-12-09 02:52:58 +0100

    solve p7

--- p7/GearStability.m
+++ p7/GearStability.m
@@ -22,7 +22,31 @@ function [alpha] = GearStability(x,y,n)
 assert(length(x)==length(y),'Les matrices x et y n''ont pas les memes dimensions !');
 n = abs(n);
 
+z = x+i*y;
+alpha = z;   % pre-allocation de alpha :-)
 
-    
+if n==1 
+    c = [1 1 1];
+elseif n==2 
+    c = [2 3 4 -1];
+elseif n==3
+    c = [6 11 18 -9 2];
+elseif n==4 
+    c = [12 25 48 -36 16 -3];
+elseif n==5
+    c = [60 137 300 -300 200 -75 12];
+else
+    c = [60 147 360 -450 400 -225 72 -10];
+end
+
+
+for k=1:size(z,1)
+    for l =1:size(z,2)
+        coeffs = [z(k,l)*c(1)-c(2), c(3:end)];
+        r = roots(coeffs);
+        alpha(k,l) = max(abs(r));
+    end
+end
+
 
 end
